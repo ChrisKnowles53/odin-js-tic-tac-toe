52bcf313a7d95865ba8351248cdadf07ecdb4345
Chris Knowles <chris.knowles@thinkmoney.co.uk>
2023-10-23 15:36:29 +0100

    styling completed, new game button added, js file refactored

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// What is it? IIFE to keep the render message private and only accessible by displayController.renderMessage(message)
+// Why use it?  It means we can access the use of renderMessage but not change it with other other parts of the code
+
 const displayController = (() => {
   const renderMessage = (message) => {
     document.querySelector("#message").innerHTML = message;
@@ -7,25 +10,35 @@ const displayController = (() => {
   };
 })();
 
+// What is it? An IIFE module that contains functions relavent to the game.
+// render - Adds squares to the div gameboard
+// update - Updates the specific gameboard square with the current players mark
+// getGameboard - Allows access to the gameboards data without directly exposing or modifying the createGameBoard array outside of the module
+// Why use it? It means we can access the use of render, update and getGameboard outside of the module
+
 const Gameboard = (() => {
-  let gameBoard = ["", "", "", "", "", "", "", "", ""];
+  let createGameBoard = ["", "", "", "", "", "", "", "", ""];
 
   const render = () => {
-    let boardHTML = "";
-    gameBoard.forEach((square, index) => {
-      boardHTML += `<div class="square" id="square-${index}">${square}</div>`;
-    });
-    document.querySelector("#gameboard").innerHTML = boardHTML;
-    const squares = document.querySelectorAll(".square");
-    squares.forEach((square) => {
-      square.addEventListener("click", Game.handleClick);
+    const gameboardParentDiv = document.querySelector("#gameboard");
+    gameboardParentDiv.innerHTML = "";
+
+    createGameBoard.forEach((square, index) => {
+      const squareDiv = document.createElement("div");
+      squareDiv.classList.add("square");
+      squareDiv.id = `square-${index}`;
+      squareDiv.textContent = square;
+
+      squareDiv.addEventListener("click", Game.handleClick);
+      gameboardParentDiv.appendChild(squareDiv);
     });
   };
-  const update = (index, value) => {
-    gameBoard[index] = value;
+  const update = (index, mark) => {
+    createGameBoard[index] = mark;
     render();
   };
-  const getGameboard = () => gameBoard;
+
+  const getGameboard = () => createGameBoard;
 
   return {
     render,
@@ -34,6 +47,7 @@ const Gameboard = (() => {
   };
 })();
 
+// Factory to create players
 const createPlayer = (name, mark) => {
   return {
     name,
@@ -69,11 +83,11 @@ const Game = (() => {
     ) {
       gameOver = true;
       displayController.renderMessage(
-        `${players[currentPlayerIndex].name} wins`
+        `🎉🎉  ${players[currentPlayerIndex].name} wins 🎉🎉`
       );
     } else if (checkForTie(Gameboard.getGameboard())) {
       gameOver = true;
-      displayController.renderMessage("Its a tie");
+      displayController.renderMessage("👔 Its a tie 👔");
     }
     currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0;
   };
@@ -82,16 +96,22 @@ const Game = (() => {
       Gameboard.update(i, "");
     }
     Gameboard.render();
+
     gameOver = false;
     document.querySelector("#message").innerHTML = "";
+  };
+  const newGame = () => {
+    window.location.reload();
   };
   return {
     start,
     restart,
+    newGame,
     handleClick,
   };
 })();
 
+// Function to check for win by looping through each row of the array and checking if the current marker is in the relevant cells
 function checkForWin(board) {
   const winningCombinations = [
     [0, 1, 2],
@@ -105,16 +125,21 @@ function checkForWin(board) {
   ];
   for (let i = 0; i < winningCombinations.length; i++) {
     const [a, b, c] = winningCombinations[i];
+    // deconstructs the line [a,b,c] into indiviual variables ready for comparison
     if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+      // if the first position is empty do nothing if the first position has a value (X or O) then continue to check if position b has a value that matches a and then check position c.
       return true;
     }
   }
   return false;
 }
 
+// Function for tie - Checks if all the cells have content
 function checkForTie(board) {
   return board.every((cell) => cell !== "");
 }
+
+// Eventlisteners fot the three buttons
 const restartButton = document.querySelector("#restart-button");
 restartButton.addEventListener("click", () => {
   Game.restart();
@@ -123,4 +148,9 @@ restartButton.addEventListener("click", () => {
 const startButton = document.querySelector("#start-button");
 startButton.addEventListener("click", () => {
   Game.start();
+});
+
+const newGame = document.querySelector("#new-game-button");
+newGame.addEventListener("click", () => {
+  Game.newGame();
 });
